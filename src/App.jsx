5952d9900be6990c11a987cb453147cb67146024
Jose Marcano <jmarcanoy@gmail.com>
2024-04-25 16:41:34 -0400

    estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { TbNetwork } from "react-icons/tb";
 import { CardTool } from './Components/CardTool'
 
 const App = () => {
@@ -33,17 +34,17 @@ const App = () => {
       url: "#"
     },
     {
-      nombre: "Ping",
+      nombre: "Ip Scan",
       categoria: "Redes",
       url: "#"
     },
     {
-      nombre: "Ping",
+      nombre: "Convertidor de Mac",
       categoria: "Redes",
       url: "#"
     },
     {
-      nombre: "Ping",
+      nombre: "Red X",
       categoria: "Redes",
       url: "#"
     },
@@ -71,50 +72,50 @@ const App = () => {
 
   return (
     <div className='min-h-screen bg-fondo'>
-      <div>
-        <header className='bg-negro h-[300px] pl-20 pt-20 flex flex-col shadow-green-500 shadow-sm'>
-          <h1 className='text-letraG text-3xl '>Help Center</h1>
+      {/* div header */}
+      <div className='bg-negro h-[300px] flex flex-col shadow-green-500 shadow-sm'>
+        <header className='pl-20 pt-20 '>
+          <div className='flex items-center gap-2'>
+            <TbNetwork color='green' size={36} />
+            <h1 className='text-letraG text-3xl'>NetworkToolKit.com</h1>
+          </div>
+
           <p className='text-letraP w-[800px] mt-5 text-[14px]'>Aqui encontrarás diversas utilidades y recursos diseñados para facilitar la gestión y optimización de redes informáticas. Estas herramientas abarcan desde programas de monitoreo y diagnóstico de red hasta aplicaciones para la configuración y seguridad de equipos interconectados.</p>
 
-          {/* ip del cliente */}
-          <div className='mt-5 flex justify-center items-center '>
-            <h1 className='text-letraG text-4xl'>Tu ip es: {publicIpAddress}</h1>
-
-
-
-          </div>
-          <div className='flex justify-between px-5'>
-            <span className='text-letraP font-bold text-[11px] '>Pais: {countryprovider}</span>
-            <span className='text-letraP font-bold text-[11px] '>ISP Proveedor: {nameprovider}</span>
-
-          </div>
+          
         </header>
-
-        <div className='pl-20 pt-20'>
-          <div>
-            <h1 className='text-2xl font-semibold'>Herramientas</h1>
+          <div className='flex justify-around items-end px-5 mt-14'>
+            <span className='text-letraP font-bold text-[11px] '>Pais: {countryprovider}</span>
+            <h1 className='text-letraG text-4xl'>Tu ip es: {publicIpAddress}</h1>
+            <span className='text-letraP font-bold text-[11px] '>ISP Proveedor: {nameprovider}</span>
           </div>
-          <div className='mt-3'>
-            <div className='grid grid-cols-4 gap-4'>
-              {
-                utilidades.map((utilidad) => {
-                  return (
-                    <CardTool key={utilidad.id} utilidad={utilidad} />
+      </div>
 
-                  )
-                })
-              }
-
-            </div>
-
-          </div>
+      <div className='pl-20 pt-20'>
+        <div>
+          <h1 className='text-2xl font-semibold'>Herramientas</h1>
         </div>
+        <div className='mt-3'>
+          <div className='grid grid-cols-4 gap-4'>
+            {
+              utilidades.map((utilidad) => {
+                return (
+                  <CardTool key={utilidad.nombre} utilidad={utilidad} />
 
+                )
+              })
+            }
+
+          </div>
+
+        </div>
       </div>
 
 
 
-    </div>
+
+
+    </div >
   )
 }
 
